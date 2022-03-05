a1d586b1841c72c9f5039affd1fb177b89a62b24
kiwidreamz <73848169+kiwidreamz@users.noreply.github.com>
2022-03-05 13:55:36 +0100

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "mealplanmaker/static"),
+    os.path.join(BASE_DIR, "mealplanmaker/static/"),
 )
 
 STATIC_URL = "mealplanmaker/static/"
